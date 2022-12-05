043c1050a06698ff9bf9a1b239b383696e5a93a9
Evan Ryan <evan.hundred@gmail.com>
2022-12-05 15:39:56 -0500

    debug

---
+++ frontend/src/store/csrf.js
@@ -0,0 +1,17 @@
+const csrfFetch = async (url, options = {}) => {
+  options.method = options.method || "GET";
+  options.headers = options.headers || {};
+
+  if (options.method.toUpperCase() !== "GET") {
+    options.headers["Content-Type"] =
+      options.headers["Content-Type"] || "application/json";
+    options.headers["X-CSRF-Token"] = sessionStorage.getItem("X-CSRF-Token");
+  }
+
+  const res = await fetch(url, options);
+  if (res >= 400) throw res;
+
+  return res;
+};
+
+export default csrfFetch;
