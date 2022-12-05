05d23abb33c0bbb577159fa2e202fc3a335dc87e
Evan Ryan <evan.hundred@gmail.com>
2022-12-05 14:27:32 -0500

    implement auth backend

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,8 +1,9 @@
 class Api::SessionsController < ApplicationController
   def show
     if current_user
-      render json: { user: current_user }
-      # render 'api/users/show'
+      # render json: { user: current_user }
+      @user = current_user
+      render '/api/users/show'
     else
       render json: { user: nil }
     end
@@ -12,7 +13,7 @@ class Api::SessionsController < ApplicationController
     @user = User.find_by_credentials(params[:email],params[:password])
     if @user
       login!(@user)
-      render json: { user: current_user }
+      render '/api/users/show'
     else
       render json: { errors: ['The provided credentials were invalid.']}, status: :unauthorized
     end
--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,5 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-Jbuilder.key_format camlize: :lower
+Jbuilder.key_format camelize: :lower
 Jbuilder.deep_format_keys true
