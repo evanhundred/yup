b6ee31f54102cab32eb492fc668913a08155e6dd
Evan Ryan <evan.hundred@gmail.com>
2022-12-05 13:40:46 -0500

    implement auth

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,13 @@ class Api::UsersController < ApplicationController
   wrap_parameters include: User.attribute_names + ['password']
 
   def create
-    render json: user_params
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      render json: @user
+    else
+      render json: { errors: @user.errors.full_messages }, status: 422 # :unprocessable_entity
+    end
   end
 
   private
