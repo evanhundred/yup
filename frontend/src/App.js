70042e39058d6203bcb1392c71c31a2ee579fd64
Evan Ryan <evan.hundred@gmail.com>
2022-12-06 09:22:34 -0500

    implement LoginFormPage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
+import LoginFormPage from "./components/LoginFormPage";
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <Switch>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+    </Switch>
   );
 }
 
