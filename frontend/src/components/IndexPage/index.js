9a02722f44eda3610842d885b0be6df36a5bbb44
Evan Ryan <evan.hundred@gmail.com>
2022-12-07 16:05:35 -0500

    style index page

--- frontend/src/components/IndexPage/index.js
+++ frontend/src/components/IndexPage/index.js
@@ -1,11 +1,22 @@
 import "./IndexPage.css";
 
+const SectionHeader = () => {
+  return (
+    <div className="section-header recent-activity">
+      <h2>Recent Activity</h2>
+    </div>
+  );
+};
+
 const IndexPage = () => {
   return (
     <>
-      <div className="image-container">
-        <img src={require("../../assets/restaurant-skyline.jpg")} />
+      <div className="image-height-reset">
+        <div className="image-container">
+          <img src={require("../../assets/restaurant-skyline.jpg")} />
+        </div>
       </div>
+      <SectionHeader />
     </>
   );
 };
