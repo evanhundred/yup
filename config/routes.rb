19aa7af7fdc75c6411c0bac1c8e2497a5acf3e63
Evan Ryan <evan.hundred@gmail.com>
2022-12-08 12:51:47 -0500

    start AWS setup; implement businesses action controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   # post 'api/test', to: 'application#test'
-  namespace :api, defaults: { format: :json } do
-    resources :users, only: :create
-    resource :session, only: [:show, :create, :destroy]
-    resources :businesses, only: [:index, :show]
+  Rails.application.routes.draw do
+    namespace :api, defaults: { format: :json } do
+      resources :users, only: :create
+      resource :session, only: [:show, :create, :destroy]
+      resources :businesses, only: [:index, :show]
+    end
   end
 
   get '*path', to: 'static_pages#frontend_index'
