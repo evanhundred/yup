fa4f37400eed149b81db5e596731ebf9a4788730
Evan Ryan <evan.hundred@gmail.com>
2022-12-12 07:00:50 -0500

    refactor css and js

--- frontend/src/components/BusinessShow/index.js
+++ frontend/src/components/BusinessShow/index.js
@@ -4,32 +4,84 @@ import { useDispatch, useSelector } from "react-redux";
 import { getBusiness, fetchBusiness } from "../../store/businesses";
 import "./index.css";
 
-// debugger;
 const BusinessShow = () => {
-  // debugger;
   const dispatch = useDispatch();
   const { businessId } = useParams();
   const business = useSelector(getBusiness(businessId));
 
   useEffect(() => {
-    // debugger;
     dispatch(fetchBusiness(businessId));
   }, [businessId, dispatch]);
 
   const [isOpen, setIsOpen] = useState(true);
 
-  // ;
+  const PhotoCard = () => {
+    return (
+      <>
+        <div class="business-image">
+          <img
+            src={require("../../assets/images/1-devocion/indoor.jpg")}
+            alt="cafe - inside"
+          />
+        </div>
+        <div id="business-title-card">
+          <h1>{business.name}</h1>
+          <SecondLine />
+          <ThirdLine />
+          <FourthLine />
+        </div>
+      </>
+    );
+  };
 
-  // const ParseAbout = () => {
-  //   const businessArray = business.about.split("+++");
-  //   // businessArray
-  //   // ;
-  //   return businessArray.map((paragraph) => <p>{paragraph}</p>);
-  // };
+  const SecondLine = () => {
+    return (
+      <div id="ratings-reviews">
+        <div className="star-box">
+          <div className="box-1">
+            <span>&lowast;</span>
+          </div>
+          <div className="box-2">
+            <span>&lowast;</span>
+          </div>
+          <div className="box-3">
+            <span>&lowast;</span>
+          </div>
+          <div className="box-4">
+            <span>&lowast;</span>
+          </div>
+          <div className="box-5">
+            <span>&lowast;</span>
+          </div>
+        </div>
+        <div className="review-count">561 reviews</div>
+      </div>
+    );
+  };
 
-  // const businessImage =
+  const ThirdLine = () => {
+    return (
+      <div className="third-line">
+        <div className="check-circle">
+          <img
+            src={require("../../assets/images/blue-check.png")}
+            className="check-symbol"
+            alt="claimed - check mark"
+          />
+        </div>
+        <p>Claimed</p>
+        <p>· {business.price} ·</p>
+        <p>{business.category}</p>
+        <a href="#">
+          <div class="edit-button-container">
+            <div className="button-box"> </div>
+            <div className="edit-text">Edit</div>
+          </div>
+        </a>
+      </div>
+    );
+  };
 
-  // const isOpen = () => {};
   const HoursDiv = () => {
     let openText;
     let openTextColor;
@@ -42,7 +94,6 @@ const BusinessShow = () => {
     }
 
     const hoursRange = business.openAt + " - " + business.closedAt;
-    // console.log(business.openAt);
 
     return (
       <>
@@ -64,6 +115,48 @@ const BusinessShow = () => {
     return (
       <div className="fourth-line">
         <HoursDiv />
+      </div>
+    );
+  };
+
+  const MainContent = () => {
+    return (
+      <div className="reset-height">
+        <div className="main-content-container">
+          <ContentNavBar />
+
+          {/* menu */}
+          <MenuCard />
+
+          {/* Location & hours */}
+          <LocationAndHours />
+
+          {/* Other xxx nearby - AD*/}
+
+          {/* Amenities and more */}
+          {/* <Amenities /> */}
+
+          {/* Help improve yelp */}
+          {/* <HelpImprove /> */}
+
+          {/* about the business */}
+          {/* <AboutCard /> */}
+
+          {/* ask the community */}
+          {/* <QuestionsCard /> */}
+
+          {/* recommended reviews */}
+          {/* <Recommended /> */}
+
+          {/* collections containing */}
+          {/* <Collections /> */}
+
+          {/* {/* people also view */}
+          {/* <AlsoViewed /> */}
+
+          {/* footer */}
+          {/* <Footer /> */}
+        </div>
       </div>
     );
   };
@@ -173,97 +266,8 @@ const BusinessShow = () => {
 
   return (
     <>
-      {/* <p>{JSON.stringify(business)}</p> */}
-      <div class="business-image">
-        <img
-          src={require("../../assets/images/1-devocion/indoor.jpg")}
-          alt="cafe - inside"
-        />
-      </div>
-      <div id="business-title-card">
-        <h1>{business.name}</h1>
-        <div id="ratings-reviews">
-          <div className="star-box">
-            <div className="box-1">
-              <span>&lowast;</span>
-            </div>
-            <div className="box-2">
-              <span>&lowast;</span>
-            </div>
-            <div className="box-3">
-              <span>&lowast;</span>
-            </div>
-            <div className="box-4">
-              <span>&lowast;</span>
-            </div>
-            <div className="box-5">
-              <span>&lowast;</span>
-            </div>
-          </div>
-          <div className="review-count">561 reviews</div>
-        </div>
-        <div className="third-line">
-          <div className="check-circle">
-            <img
-              src={require("../../assets/images/blue-check.png")}
-              className="check-symbol"
-              alt="claimed - check mark"
-            />
-          </div>
-          <p>Claimed</p>
-          <p>· {business.price} ·</p>
-          <p>{business.category}</p>
-          <a href="#">
-            <div class="edit-button-container">
-              {/* <img
-                src={require("../../assets/images/box-transparency-and-translucency-glass-color-text-box.png")}
-                alt="edit button"
-                className="button-box"
-              /> */}
-              <div className="button-box"> </div>
-              <div className="edit-text">Edit</div>
-            </div>
-          </a>
-        </div>
-        <FourthLine />
-      </div>
-      <div className="reset-height">
-        <div className="main-content-container">
-          <ContentNavBar />
-
-          {/* menu */}
-          <MenuCard />
-
-          {/* Location & hours */}
-          <LocationAndHours />
-
-          {/* Other xxx nearby - AD*/}
-
-          {/* Amenities and more */}
-          {/* <Amenities /> */}
-
-          {/* Help improve yelp */}
-          {/* <HelpImprove /> */}
-
-          {/* about the business */}
-          {/* <AboutCard /> */}
-
-          {/* ask the community */}
-          {/* <QuestionsCard /> */}
-
-          {/* recommended reviews */}
-          {/* <Recommended /> */}
-
-          {/* collections containing */}
-          {/* <Collections /> */}
-
-          {/* {/* people also view */}
-          {/* <AlsoViewed /> */}
-
-          {/* footer */}
-          {/* <Footer /> */}
-        </div>
-      </div>
+      <PhotoCard />
+      <MainContent />
     </>
   );
 };
