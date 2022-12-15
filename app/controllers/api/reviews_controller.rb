2425722dfefa423375949dbbc270ec7a1868d24f
Evan Ryan <evan.hundred@gmail.com>
2022-12-15 09:46:44 -0500

    write backend for reviews

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,6 +1,7 @@
 class Api::ReviewsController < ApplicationController
     def create
         @review = Review.new(review_params)
+        @review.author_id = params[:user_id]
         unless @review.save
             render json: { errors: @review.errors.full_messages }, status: 422 # :unprocessable_entity
         end
@@ -8,14 +9,20 @@ class Api::ReviewsController < ApplicationController
 
     def destroy
         review = Review.find(params[:id])
-
+        unless review.destroy
+            render json: { errors: @review.errors.full_messages }, status: 422 # :unprocessable_entity
+        end
     end
 
     def edit
-
+        @review = Review.find(params[:id])
+        if current_user == User.find(@review.author_id)
+            render :edit
+        end
     end
 
     def update
+        @review = Review.find(params[:id])
 
     end
 
