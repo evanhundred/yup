87e083f1c1b523745d1df28e922d0ed3db935fc0
Evan Ryan <evan.hundred@gmail.com>
2023-04-05 05:40:25 -0400

    integrate location and category search

---
+++ frontend/src/components/SearchResults/FormatSearchQuery.js
@@ -0,0 +1,9 @@
+import StateAbbreviations from "./states.txt";
+
+const FormatSearchQuery = () => {
+  // const stateAbbreviations = () => {
+  //   "";
+  // };
+};
+
+export default FormatSearchQuery;
