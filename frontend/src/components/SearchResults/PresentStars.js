1d445688b154c6bc58986898a3dfd50a59bfb4eb
Evan Ryan <evan.hundred@gmail.com>
2023-04-25 16:11:56 -0400

    implement rating calculator - wip

--- frontend/src/components/SearchResults/PresentStars.js
+++ frontend/src/components/SearchResults/PresentStars.js
@@ -1,5 +1,22 @@
 const PresentStars = ({ business }) => {
   if (business.reviews.length === 0) return null;
+
+  let avgRating;
+  if (business.reviews.length === 1) {
+    avgRating = business.reviews[0].rating;
+  } else {
+    let sumRatings = 0;
+    for (let i = 0; i < business.reviews.length; i++) {
+      sumRatings += business.reviews[i].rating;
+    }
+    avgRating = sumRatings / business.reviews.length;
+
+    // step 1. chop off decimals after the first 2
+    let avgRating2dp = Math.round(avgRating * 100) / 100;
+
+    let finalAvg;
+    if (Number.isInteger(avgRating2dp)) finalAvg = avgRating2dp;
+  }
 };
 
 export default PresentStars;
