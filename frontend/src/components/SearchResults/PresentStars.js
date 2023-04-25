7b80ed15d2f3d22da0a412a2d9f0ab8cf7e02d70
Evan Ryan <evan.hundred@gmail.com>
2023-04-25 16:20:48 -0400

    implement ratings cruncher in PresentStars component - need to test

--- frontend/src/components/SearchResults/PresentStars.js
+++ frontend/src/components/SearchResults/PresentStars.js
@@ -14,9 +14,19 @@ const PresentStars = ({ business }) => {
     // step 1. chop off decimals after the first 2
     let avgRating2dp = Math.round(avgRating * 100) / 100;
 
-    let finalAvg;
-    if (Number.isInteger(avgRating2dp)) finalAvg = avgRating2dp;
+    let reducedAvg; // average ratings "rounded" to the nearest .5
+    if (Number.isInteger(avgRating2dp)) reducedAvg = avgRating2dp;
+    else {
+      let avgDecimals = avgRating2dp - Math.floor(avgRating2dp);
+      if (avgDecimals < 0.25) reducedAvg = Math.floor(avgRating2dp);
+      else if (avgDecimals < 0.75) reducedAvg = Math.floor(avgRating2dp) + 0.5;
+      else reducedAvg = Math.ceil(avgRating2dp);
+    }
+    // CHECK THIS SYNTAX ^^^^^ I HAVE SEEN IT USED BUT NOT TESTED IT
   }
+  // now we have the average ratings for the business, rounded to the nearest half-star.
+  // TODO: use this tool to calculate business ratings, and format output to be presented
+  // as stars. 04-25-23
 };
 
 export default PresentStars;
