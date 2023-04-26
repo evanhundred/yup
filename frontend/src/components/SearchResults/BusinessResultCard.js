094ecdced8432a18c3a27d95ba21bf4301971847
Evan Ryan <evan.hundred@gmail.com>
2023-04-26 09:23:48 -0400

    configure searchResultCard styling

--- frontend/src/components/SearchResults/BusinessResultCard.js
+++ frontend/src/components/SearchResults/BusinessResultCard.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./BusinessResultCard.css";
 import PresentStars from "./PresentStars";
 
@@ -5,21 +6,23 @@ const BusinessResultCard = ({ business, idx }) => {
   // if (!business) return null;
   debugger;
   return (
-    <div className="business-card-container">
-      <div className="business-photo-container">
-        <img src={business.imageUrls[5]} alt="delicious item" />
-      </div>
-      <div className="business-info">
-        <div className="business-title">
-          <p>
-            {idx}. {business.name}
-          </p>
+    <Link to={`/businesses/${business.id}`}>
+      <div className="business-card-container">
+        <div className="business-photo-container">
+          <img src={business.imageUrls[5]} alt="delicious item" />
         </div>
-        <div className="star-rating">
-          <PresentStars business={business} />
+        <div className="business-info">
+          <div className="business-title">
+            <p>
+              {idx}. {business.name}
+            </p>
+          </div>
+          <div className="star-rating">
+            <PresentStars business={business} />
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
