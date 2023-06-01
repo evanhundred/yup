ee2a582aba458244142a60f937870b26f32e84c4
Evan Ryan <evan.hundred@gmail.com>
2023-06-01 18:53:53 -0400

    actually fixed redirect to / on invalid businessId params

--- .todo/may31.md
+++ .todo/may31.md
@@ -6,7 +6,7 @@
 
 business show:
 
-1. redirect to "/" on incoming invalid business id's
+1. _(complete)_ redirect to "/" on incoming invalid business id's
    - compare params number to businesses.length; if too high, load `business not found`
    - **FIXED**: redirect to index. no errors should be presented to the user, because it results from an invalid url, which may result from various circumstances.
 2. fix dead links - enumerated in previous todo's
--- frontend/src/components/BusinessShow/index.js
+++ frontend/src/components/BusinessShow/index.js
@@ -3,16 +3,15 @@ import { useParams, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
   getBusiness,
-  getBusinesses,
   fetchBusiness,
-  receiveErrors
+  fetchBusinesses
 } from "../../store/businesses";
 import "./index.css";
 import TitleCard from "./TitleCard";
 import MainContent from "./MainContent";
 
 const BusinessShow = ({ props }) => {
-  console.log(useParams());
+  // console.log(useParams());
   const dispatch = useDispatch();
   const history = useHistory();
   const { businessId } = useParams();
@@ -21,34 +20,17 @@ const BusinessShow = ({ props }) => {
   const businessCount = useSelector(
     (state) => Object.values(state.businesses).length
   );
-  // const errors = useSelector((state) => state.errors);
 
-  // if !business, redirect to home
-  //  incorrect: this redirects on every refresh
-  //  if (!business) history.push(`/`);
-  // if ()
-
-  // let fetched = false;
-  // let data;
   useEffect(() => {
     dispatch(fetchBusiness(businessId));
   }, [businessId, dispatch]);
 
-  // console.log(data);
+  useEffect(() => {
+    dispatch(fetchBusinesses());
+  }, [dispatch]);
 
-  // useEffect(() => {
-  //   const fetchBiz = async () => {
-  //     dispatch(fetchBusiness(businessId));
-  //     try {
-  //       const response = await
-  //     }
-  //   }
-  // }, [businessId, dispatch]);
-
-  // debugger;
-  // if (errors) return <div className="errors">{errors}</div>;
-  if (businessCount < businessId) history.push(`/`);
-  // return <div className="not-found">business not found</div>;
+  if (businessCount > 1 && businessCount < parseInt(businessId))
+    history.push(`/`);
   if (!business) return <div className="loading">loading...</div>;
 
   // if (props === "goToReviews") {
