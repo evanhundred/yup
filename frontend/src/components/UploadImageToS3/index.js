44116cc2e9ba3ade915b106f60947b2a6767a517
Evan Ryan <evan.hundred@gmail.com>
2023-06-08 18:46:46 -0400

    prepare to create biz-user-photo table for AWS interface

--- frontend/src/components/UploadImageToS3/index.js
+++ frontend/src/components/UploadImageToS3/index.js
@@ -5,9 +5,37 @@ import { dirnameFromMetaUrl } from "libs/utils/util-fs.js";
 import { promptForText, promptToContinue } from "libs/utils/util-io.js";
 import { wrapText } from "libs/utils/util-string.js";
 
-import { S3Client, PutObjectCommand } from "@aws-sdk/client/s3";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
-const s3Client = new S3Client({});
+// const bareBonesS3 = new S3Client({...});
+// await bareBonesS3.send(new)
+
+// prev2
+
+// const s3Client = new S3Client({});
+// export const uploadFilesToBucket = async ({ bucketName, folderPath }) => {
+//   console.log(`Uploading files from ${folderPath}\n`);
+//   const keys = readdirSync(folderPath);
+//   const files = keys.map((key) => {
+//     const filePath = `${folderPath}/${key}`;
+//     const fileContent = readFileSync(filePath);
+//     return {
+//       Key: key,
+//       Body: fileContent
+//     };
+//   });
+
+//   for (let file of files) {
+//     await s3Client.sent(
+//       new PutObjectCommand({
+//         Bucket: bucketName,
+//         Body: file.Body,
+//         Key: file.Key
+//       })
+//     );
+//     console.log(`${file.Key} uploaded successfully.`);
+//   }
+// };
 
 // export const uploadFilesToBucket
 
