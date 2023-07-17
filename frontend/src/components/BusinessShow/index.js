dc07dce6b1273abe705c3116977d73e802141837
Evan Ryan <evan.hundred@gmail.com>
2023-07-17 19:30:28 -0400

    implement auto adjust css on window resize - business show error page

--- frontend/src/components/BusinessShow/index.js
+++ frontend/src/components/BusinessShow/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getBusiness, fetchBusiness } from "../../store/businesses";
@@ -13,23 +13,43 @@ const BusinessShow = ({ props }) => {
 
   const business = useSelector(getBusiness(businessId));
 
+  // let layoutWidth;
+  // useEffect(() => {
+  //   if (window.innerWidth >= 932) layoutWidth = "full-size";
+  //   else layoutWidth = "narrow-size";
+  // }, []);
+
   useEffect(() => {
     dispatch(fetchBusiness(businessId)); // .catch((errors) => console.log(errors));
   }, [businessId, dispatch]);
+
+  console.log("innerWidth:");
+  console.log(window.innerWidth);
+
+  // const windowWidth = window.innerWidth; // 932 min size for full render
+  // if (windowWidth < 932)
+  const [windowSize, setWindowSize] = useState(0);
+  let layoutWidth;
+  const updateSize = () => {
+    setWindowSize(window.innerWidth);
+    layoutWidth = window.innerWidth >= 600 ? "full-size" : "narrow-size";
+  };
+  window.addEventListener("resize", updateSize);
+  layoutWidth = window.innerWidth >= 600 ? "full-size" : "narrow-size";
 
   if (!business) return <div className="loading">loading...</div>;
 
   if (business.status === 500)
     return (
-      <div id="invalid-business-container">
-        <div className="left-side">
+      <div id="invalid-business-container" className={layoutWidth}>
+        <div className={`left-side ${layoutWidth}`}>
           <h2>We're sorry. We can't find the page you're looking for.</h2>
           <h3>
             Please try a new <Link to="/search">search</Link>.
           </h3>
         </div>
-        <div className="right-side">
-          <img src={webSpider} alt="web spider" />
+        <div className={`right-side ${layoutWidth}`}>
+          <img src={webSpider} alt="web spider" title="image by freepik" />
         </div>
       </div>
     );
