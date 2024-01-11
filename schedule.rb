a066b1781dd7bcd81e95631309f78074f1b3f670
Evan Ryan <107318423+evanhundred@users.noreply.github.com>
2024-01-11 08:05:22 -0500

    add font family for load more button

---
+++ schedule.rb
@@ -0,0 +1,6 @@
+every 14.minutes do # 1.minute 1.day 1.week 1.month 1.year is also supported
+  # the following tasks are run in parallel (not in sequence)
+	# 	runner "MyModel.some_process"
+	#   rake "my:rake:task"
+	#   command "/usr/bin/my_great_command"
+end
