ba917d0f0d6b688cd95d65e8ec9e3c5f14de0074
Evan Ryan <107318423+evanhundred@users.noreply.github.com>
2024-02-13 13:49:50 -0500

    cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,21 +31,6 @@ class ApplicationController < ActionController::API
         end
     end
 
-    # def test
-    #     # debugger
-    #     if params.has_key?(:login)
-    #         login!(User.first)
-    #     elsif params.has_key?(:logout)
-    #         logout!
-    #     end
-
-    #     if current_user
-    #         render json: { user: current_user.slice('email', 'name', 'session_token') }
-    #     else
-    #         render json: ['No current user']
-    #     end
-    # end
-
     private
 
     def snake_case_params
