962c2413328a591df6477049c2c0169080953b78
Evan Ryan <107318423+evanhundred@users.noreply.github.com>
2024-03-28 19:03:01 -0400

    fix cyclomatic complexity - wip

--- app/controllers/biz_photo_boxes_controller.rb
+++ app/controllers/biz_photo_boxes_controller.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
+# provide method for BizPhotoBox
 class BizPhotoBoxesController < ApplicationController
-    def show
-        @biz_photo_box = BizPhotoBox.find(params[:business_id])
-        if @biz_photo_box
-            render :show
-        else
-            render json: @biz_photo_box.errors.full_messages, status: 404
-        end
+  def show
+    @biz_photo_box = BizPhotoBox.find(params[:business_id])
+    if @biz_photo_box
+      render :show
+    else
+      render json: @biz_photo_box.errors.full_messages, status: 404
     end
+  end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
+# provides index page method
 class StaticPagesController < ActionController::Base
-    def frontend_index
-        render file: Rails.root.join('public', 'index.html')
-    end
+  def frontend_index
+    render file: Rails.root.join('public', 'index.html')
+  end
 end
