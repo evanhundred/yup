7fe36d3223f127e54e0e4bb1b556c50b5118bdaf
Evan Ryan <107318423+evanhundred@users.noreply.github.com>
2024-07-15 19:16:13 -0400

    wip nav menu toggle

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,16 +1,13 @@
-import { useState, useEffect, useRef } from "react";
-import { useDispatch } from "react-redux";
-import * as sessionActions from "../../store/session";
-import "./ProfileButton.css";
-import profileIcon from "../../assets/images/profile.png";
-import logoutButton from "../../assets/images/logout.png";
+import { useState, useEffect, useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import * as sessionActions from '../../store/session';
+import './ProfileButton.css';
+import profileIcon from '../../assets/images/profile.png';
+import logoutButton from '../../assets/images/logout.png';
 
 const Carrot = () => (
-  <div
-    className="profile-picture"
-    style={{ color: "orange", fontSize: "40px" }}
-  >
-    <i className="fa-solid fa-carrot"></i>
+  <div className='profile-picture' style={{ color: 'orange', fontSize: '40px' }}>
+    <i className='fa-solid fa-carrot'></i>
   </div>
 );
 
@@ -19,24 +16,34 @@ const ProfileButton = ({ user }) => {
   const [showMenu, setShowMenu] = useState(false);
   const dropdownRef = useRef(null);
 
-  const openMenu = () => {
-    if (showMenu) return;
-    setShowMenu(true);
-  };
-
-  const closeMenu = (e) => {
+  const toggleMenu = (e) => {
     if (!dropdownRef.current.contains(e.target)) {
-      setShowMenu(false);
+      setShowMenu(!showMenu);
+      document.addEventListener('click', toggleMenu);
+
+      // todo:
+      // create removeEventListener for menu close
     }
   };
 
-  useEffect(() => {
-    if (!showMenu) return;
+  // const openMenu = () => {
+  //   if (showMenu) return;
+  //   setShowMenu(true);
+  // };
 
-    document.addEventListener("click", closeMenu);
+  // const closeMenu = (e) => {
+  //   if (!dropdownRef.current.contains(e.target)) {
+  //     setShowMenu(false);
+  //   }
+  // };
 
-    return () => document.removeEventListener("click", closeMenu);
-  }, [showMenu]);
+  // useEffect(() => {
+  //   if (!showMenu) return;
+
+  //   document.addEventListener("click", closeMenu);
+
+  //   return () => document.removeEventListener("click", closeMenu);
+  // }, [showMenu]);
 
   const handleLogoutClick = (e) => {
     logout(e);
@@ -48,31 +55,28 @@ const ProfileButton = ({ user }) => {
   };
 
   return (
-    <div id="profile-menu-button">
-      <div className="profile-image-container" onClick={(e) => openMenu(e)}>
+    <div id='profile-menu-button'>
+      <div className='profile-image-container' onClick={toggleMenu}>
         <Carrot />
       </div>
       {showMenu && (
-        <ul className="profile-dropdown" ref={dropdownRef}>
-          <li className="user-options-container">
-            <div className="first-row profile-dropdown-row">
-              <div className="profile-icon">
-                <img src={profileIcon} alt="your profile" />
+        <ul className='profile-dropdown' ref={dropdownRef}>
+          <li className='user-options-container'>
+            <div className='first-row profile-dropdown-row'>
+              <div className='profile-icon'>
+                <img src={profileIcon} alt='your profile' />
               </div>
-              <div className="profile-dropdown-option">
+              <div className='profile-dropdown-option'>
                 <p>{user.name}</p>
               </div>
             </div>
           </li>
-          <li className="logout-button-container">
-            <div
-              className="logout-button profile-dropdown-row"
-              onClick={handleLogoutClick}
-            >
-              <div className="profile-icon">
-                <img src={logoutButton} alt="logout" />
+          <li className='logout-button-container'>
+            <div className='logout-button profile-dropdown-row' onClick={handleLogoutClick}>
+              <div className='profile-icon'>
+                <img src={logoutButton} alt='logout' />
               </div>
-              <div className="profile-dropdown-option">
+              <div className='profile-dropdown-option'>
                 <p>Log Out</p>
               </div>
             </div>
