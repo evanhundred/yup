01c13f996cdb43b3487d523212c989408eb6dd30
Evan Ryan <107318423+evanhundred@users.noreply.github.com>
2025-10-20 09:27:24 -0400

    refactor PhotoBlock.jsx - stubImages

--- frontend/src/components/BusinessShow/TitleCard/PhotoBlock.jsx
+++ frontend/src/components/BusinessShow/TitleCard/PhotoBlock.jsx
@@ -1,47 +1,41 @@
 const PhotoBlock = ({ business }) => {
   let defaultImages;
-  if (business.stub === "true") {
-    const importAll = (r) => {
+  if (business.stub === 'true') {
+    const importAll = (stubImages) => {
       let images = {};
-      r.keys().map((item) => {
-        return (images[item.replace("./", "")] = r(item));
+      stubImages.keys().map((item) => {
+        return (images[item.replace('./', '')] = stubImages(item));
       });
       return images;
     };
-    defaultImages = importAll(
-      require.context("../../../assets/images/stub/", false, /\S/)
-    );
+    defaultImages = importAll(require.context('../../../assets/images/stub/', false, /\S/));
   }
 
   const getImage = (idx) => {
-    if (business.stub === "true") {
+    if (business.stub === 'true') {
       return defaultImages[`${idx}.jpeg`];
     }
     return business.imageUrls[idx];
   };
   return (
-    <div
-      className={`business-image-container${
-        business.stub === "true" ? " stub" : ""
-      }`}
-    >
-      <div className="business-image one">
-        <img src={getImage(0)} alt="delicious items" />
+    <div className={`business-image-container${business.stub === 'true' ? ' stub' : ''}`}>
+      <div className='business-image one'>
+        <img src={getImage(0)} alt='delicious items' />
       </div>
 
-      <div className="business-image two">
-        <img src={getImage(4)} alt="fantastic store" />
+      <div className='business-image two'>
+        <img src={getImage(4)} alt='fantastic store' />
       </div>
 
-      <div className="business-image three">
-        <img src={getImage(1)} alt="more delicious items" />
+      <div className='business-image three'>
+        <img src={getImage(1)} alt='more delicious items' />
       </div>
 
-      <div className="business-image four">
-        <img src={getImage(2)} alt="more delicious deliciousness" />
+      <div className='business-image four'>
+        <img src={getImage(2)} alt='more delicious deliciousness' />
       </div>
-      <div className="business-image five">
-        <img src={getImage(3)} alt="more more delicious deliciousness" />
+      <div className='business-image five'>
+        <img src={getImage(3)} alt='more more delicious deliciousness' />
       </div>
     </div>
   );
